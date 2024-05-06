115bdcb47d5b3da59cd86ca5313bfc4626d54b01
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-06 14:09:23 +0200

    [UPD] lista

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const listaSpesa = ["uova", "latte", "cereali", "estathè", "pasta",];
+let count = 0;
+
+const ul = document.getElementById("lista");
+
+while (count !== listaSpesa.length) {
+    const li = document.createElement("li");
+    li.append(listaSpesa[count]);
+    ul.append(li);
+    count++;
+}
+
+/*
+do{
+    const li = document.createElement("li");
+    li.append(listaSpesa[count]);
+    ul.append(li);
+    count++;
+} while (count !== listaSpesa.length);
+*/
